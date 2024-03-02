62e6f4a5a222ed963b78e4fb1703f43b45dbce05
mschmitt1999 <majid.schmitt@gmail.com>
2024-03-02 21:55:20 +0100

    Update script.js
    Refactored some code, too much static bs in ScaleView

--- script.js
+++ script.js
@@ -1,76 +1,88 @@
 class Scale {
-    static allNotesMap = new Map();
-    static scalesMap = new Map();
+    static allNotesMap;
+    static scalesMap;
     rootNoteKeyDouble;
     scaleNameString;
     scaleNotes = [];    
     
     constructor(scaleString, rootNoteKeyDouble){
+        
         Scale.initializeNotesMap();
         Scale.initializeScalesMap();
-        this.scaleNameString = 'Ionisch';
-        this.rootNoteKeyDouble = 1.0;
+        this.setScaleNameString('Ionisch');
+        this.setRootNoteKeyDouble(1.0);
     }
 
     static initializeNotesMap(){
         // Return map of: (key: double, value: String)
         //Initialize @allNotesMap, that contains all notes as values with their represantative double...
-        Scale.allNotesMap.set(1.0, 'C');
-        Scale.allNotesMap.set(1.5, 'C#');
-        Scale.allNotesMap.set(2.0, 'D');
-        Scale.allNotesMap.set(2.5, 'D#');
-        Scale.allNotesMap.set(3.0, 'E');
-        Scale.allNotesMap.set(3.5, 'F');
-        Scale.allNotesMap.set(4.0, 'F#');
-        Scale.allNotesMap.set(4.5, 'G');
-        Scale.allNotesMap.set(5.0, 'G#');
-        Scale.allNotesMap.set(5.5, 'A');
-        Scale.allNotesMap.set(6.0, 'A#');
-        Scale.allNotesMap.set(6.5, 'H');
+        Scale.setAllNotesMap(new Map([ 
+            [1.0, 'C'],
+            [1.5, 'C#'],
+            [2.0, 'D'],
+            [2.5, 'D#'],
+            [3.0, 'E'],
+            [3.5, 'F'],
+            [4.0, 'F#'],
+            [4.5, 'G'],
+            [5.0, 'G#'],
+            [5.5, 'A'],
+            [6.0, 'A#'],
+            [6.5, 'H']
+        ]));
     }
     
     
     static initializeScalesMap(){
-        Scale.scalesMap.set('Ionisch', [7,2,6]);
-        Scale.scalesMap.set('Dorisch', [7,1,5]);
-        Scale.scalesMap.set('Phrygisch', [7,0,4]);
-        Scale.scalesMap.set('Lydisch', [7,3,7]);
-        Scale.scalesMap.set('Mixolydisch', [7,2,5]);
-        Scale.scalesMap.set('Äolisch', [7,1,4]);
-        Scale.scalesMap.set('Lokrisch', [7,0,3]);
-        Scale.scalesMap.set('DurPentatonik',[7,2,6]);
-        Scale.scalesMap.set('MolPentatonik',[7,1,4]);
+        Scale.setScalesMap(new Map([
+            ['Ionisch', [7, 2, 6]],
+            ['Dorisch', [7, 1, 5]],
+            ['Phrygisch', [7, 0, 4]],
+            ['Lydisch', [7, 3, 7]],
+            ['Mixolydisch', [7, 2, 5]],
+            ['Äolisch', [7, 1, 4]],
+            ['Lokrisch', [7, 0, 3]],
+            ['DurPentatonik', [7, 2, 6]],
+            ['MolPentatonik', [7, 1, 4]],
+        ]));
     }
     
     calculateScale(){
         let noteKeyDouble = this.getRootNoteKeyDouble();
-        let lenghtOfScale = Scale.scalesMap.get(this.scaleNameString).at(0);
-        let halfSteps = Scale.scalesMap.get(this.scaleNameString).slice(1,lenghtOfScale);
+        let lenghtOfScale = Scale.getScalesMap().get(this.getScaleNameString()).at(0);
+        let halfSteps = Scale.getScalesMap().get(this.getScaleNameString()).slice(1,lenghtOfScale);
         let scaleNotesString = '';
-        this.scaleNotes = [];
+        this.setScaleNotes([]);
+
         for (let i=0; i<lenghtOfScale; i++){
-            if(!(this.scaleNameString=='DurPentatonik' && (i==3 || i==6)) && !(this.scaleNameString=='MolPentatonik' && (i==1 || i==5))){
-            scaleNotesString = scaleNotesString.concat(Scale.allNotesMap.get(noteKeyDouble),' | '); 
-            this.scaleNotes.push([noteKeyDouble, Scale.allNotesMap.get(noteKeyDouble)]);
+            if(!(this.getScaleNameString()=='DurPentatonik' && (i==3 || i==6)) && !(this.getScaleNameString()=='MolPentatonik' && (i==1 || i==5))){
+                scaleNotesString = scaleNotesString.concat(Scale.getAllNotesMap().get(noteKeyDouble),' | '); 
+                this.getScaleNotes().push([noteKeyDouble, Scale.getAllNotesMap().get(noteKeyDouble)]);
             }
             if(halfSteps.includes(i)){
-                    noteKeyDouble += 0.5;
-                }
-                else{
-                    noteKeyDouble +=1.0;
-                }
-                if(noteKeyDouble >= lenghtOfScale){
-                    noteKeyDouble -= lenghtOfScale-1;
-                }
+                noteKeyDouble += 0.5;
+            }
+            else{
+                noteKeyDouble +=1.0;
+            }
+            if(noteKeyDouble >= lenghtOfScale){
+                noteKeyDouble -= lenghtOfScale-1;
+            }
         }
         return scaleNotesString;
+    }
+
+    calculateScaleString(){
+        this.getScaleNotes.forEach(element => {
+            element[0]            
+        });
     }
 
     setAndCalculateRandomScale(){
         let scaleKeyArray = Array.from(Scale.scalesMap.keys());
         let noteKeyArray = Array.from(Scale.allNotesMap.keys());
-        this.scaleNameString = scaleKeyArray.at(Math.floor(Math.random()*scaleKeyArray.length));
-        this.rootNoteKeyDouble = noteKeyArray.at(Math.floor(Math.random() * noteKeyArray.length))
+        this.setScaleNameString(scaleKeyArray.at(Math.floor(Math.random()*scaleKeyArray.length)));
+        this.setRootNoteKeyDouble(noteKeyArray.at(Math.floor(Math.random() * noteKeyArray.length)));
         return this.calculateScale();  
     }
 
@@ -92,10 +104,10 @@ class Scale {
     setScaleNotes(scaleNotes) {
         this.scaleNotes = scaleNotes;
     }
-    static getNotesMap() {
+    static getAllNotesMap() {
         return Scale.allNotesMap;
     }
-    static setNotesMap(notesMap) {
+    static setAllNotesMap(notesMap) {
         Scale.allNotesMap = notesMap;
     }
     static getScalesMap() {
@@ -107,143 +119,155 @@ class Scale {
 }
 
 
-
-
-
-
-
 class ScaleView {
-    static showOrHideScaleButton = document.getElementById('showOrHideScaleButton');
     randomScaleButton;
-    static swtichSVGButton = document.getElementById('switchSVG');
-    static isKeyboardSvgShown = true;
-
-    static scalesSelectbox = document.getElementById('selectedScales');
-    static notesSelectbox = document.getElementById('selectedNotes');
-    static scaleTextView = document.getElementById('scaleTextView');
-    static scaleClass = new Scale();
-    static noteColorMap = new Map();
-    static showScaleBoolean = false;
-    static keyboardSVG = document.getElementById('keyboardSVG');
-    static guitarSVG = document.getElementById('guitarSVG');
-    static svgContainerDiv = document.getElementById('svg-container');
+    swtichSVGButton;
+    isKeyboardSvgShown;
+    showOrHideScaleButton;
+    scalesSelectbox;
+    notesSelectbox;
+    scaleTextView;
+    scaleClass;
+    noteColorMap;
+    showScaleBoolean;
+    keyboardSVG;
+    guitarSVG;
+    svgContainerDiv;
 
     constructor(){
-        ScaleView.showKeyboardSVG();
-        this.initializeNoteColorMap();
-        ScaleView.scalesSelectbox.onchange = function(){
-            ScaleView.scaleClass.setScaleNameString(this.value);
-            if(ScaleView.showScaleBoolean){
-                ScaleView.showScale();
-            }
-        };
-        
-        ScaleView.notesSelectbox.onchange = function(){
-            ScaleView.scaleClass.rootNoteKeyDouble = parseFloat(this.value);
-            if(ScaleView.showScaleBoolean){
-                ScaleView.showScale();
-            }
-        };
-        
-        ScaleView.showOrHideScaleButton.addEventListener('click', function(){
-            if(ScaleView.showScaleBoolean){
-                ScaleView.hideScale();
-                ScaleView.showScaleBoolean = false;
-            }
-            else{
-                ScaleView.showScale();
-                ScaleView.showScaleBoolean = true;
-            }
-
-        })
+        this.swtichSVGButton = document.getElementById('switchSVG');
+        this.isKeyboardSvgShown = true;
+        this.showOrHideScaleButton = document.getElementById('showOrHideScaleButton');
+        this.scalesSelectbox = document.getElementById('selectedScales');
+        this.notesSelectbox = document.getElementById('selectedNotes');
+        this.scaleTextView = document.getElementById('scaleTextView');
+        this.scaleClass = new Scale();
+        this.noteColorMap = new Map();
+        this.showScaleBoolean = false;
+        this.keyboardSVG = document.getElementById('keyboardSVG');
+        this.guitarSVG = document.getElementById('guitarSVG');
+        this.svgContainerDiv = document.getElementById('svg-container');
+    
+        this.showKeyboardSVG();
+        this.initializeNoteColorMap();       
+       
         this.randomScaleButton = document.getElementById('randomScale');
-        this.randomScaleButton.addEventListener('click', function(){
-            
-            let scaleString = ScaleView.scaleClass.setAndCalculateRandomScale();
-            if(ScaleView.showScaleBoolean){
-                ScaleView.scaleTextView.innerText = scaleString;
-                if(ScaleView.isKeyboardSvgShown){
-                    ScaleView.highlightKeyboardNotes();
-                }
-                else{
-                    ScaleView.highlightGuitarNotes();
-                }
-            }
-            ScaleView.scalesSelectbox.value = ScaleView.scaleClass.getScaleNameString();
-            ScaleView.notesSelectbox.value = ScaleView.scaleClass.rootNoteKeyDouble;
-        })
-
-        ScaleView.swtichSVGButton.addEventListener('click',function(){
-            ScaleView.swtichSVG();
-
-            let svgLabel= document.getElementById('switchSVGLabel');
-            if(ScaleView.isKeyboardSvgShown){
-                svgLabel.innerText ='Guitar';
-                if(ScaleView.showScaleBoolean){
-                    ScaleView.highlightKeyboardNotes();
-                }
-                else{
-                    ScaleView.resetKeyboardNotes();
-                }
-            }
-            else{
-                svgLabel.innerText = 'Keyboard';
-
-                if(ScaleView.showScaleBoolean){
-                    if(ScaleView.showScaleBoolean){
-                        ScaleView.highlightGuitarNotes();
-                    }
-                }
-                else{
-                    ScaleView.resetGuitarNotes();
-                }
-            }
-        } );
+        this.randomScaleButton.addEventListener('click', () => this.randomScaleButtonClick());
+        this.swtichSVGButton.addEventListener('click', () => this.swtichSVGButtonClick());
+        this.showOrHideScaleButton.addEventListener('click', () => this.showOrHideScaleButtonClick());
+        this.scalesSelectbox.onchange = () => this.scalesSelectboxOnchange;
+        this.notesSelectbox.onchange = () => this.notesSelectboxOnchange();
     }
 
-    static showScale(){
-        ScaleView.showScaleBoolean = true;
-        ScaleView.scaleTextView.innerText = ScaleView.scaleClass.calculateScale();
-        if (ScaleView.isKeyboardSvgShown){
-            ScaleView.highlightKeyboardNotes();
+    scalesSelectboxOnchange(){
+        this.scaleClass.setScaleNameString(this.value);
+        if(this.showScaleBoolean){
+            this.showScale();
+        }   
+    }
+
+    notesSelectboxOnchange(){
+        this.scaleClass.rootNoteKeyDouble = parseFloat(this.value);
+        if(this.showScaleBoolean){
+            this.showScale();
+        }
+    }
+
+   showOrHideScaleButtonClick(){
+        if(this.showScaleBoolean){
+            this.hideScale();
+            this.showScaleBoolean = false;
         }
         else{
-            ScaleView.highlightGuitarNotes();
+            this.showScale();
+            this.showScaleBoolean = true;
+        }
+    }
+
+    randomScaleButtonClick(){
+        let scaleString = this.scaleClass.setAndCalculateRandomScale();
+        if(this.showScaleBoolean){
+            this.scaleTextView.innerText = scaleString;
+            if(this.isKeyboardSvgShown){
+                this.highlightKeyboardNotes();
+            }
+            else{
+                this.highlightGuitarNotes();
+            }
+        }
+        this.scalesSelectbox.value = this.scaleClass.getScaleNameString();
+        this.notesSelectbox.value = this.scaleClass.rootNoteKeyDouble;
+    }
+
+    swtichSVGButtonClick(){
+        this.swtichSVG();
+        let svgLabel= document.getElementById('switchSVGLabel');
+        if(this.isKeyboardSvgShown){
+            svgLabel.innerText ='Guitar';
+            if(this.showScaleBoolean){
+                this.highlightKeyboardNotes();
+            }
+            else{
+                this.resetKeyboardNotes();
+            }
+        }
+        else{
+            svgLabel.innerText = 'Keyboard';
+
+            if(this.showScaleBoolean){
+                if(this.showScaleBoolean){
+                    this.highlightGuitarNotes();
+                }
+            }
+            else{
+                this.resetGuitarNotes();
+            }
+        }
+    }
+
+    showScale(){
+        this.showScaleBoolean = true;
+        this.scaleTextView.innerText = this.scaleClass.calculateScale();
+        if (this.isKeyboardSvgShown){
+            this.highlightKeyboardNotes();
+        }
+        else{
+            this.highlightGuitarNotes();
         }
         document.getElementById('showScaleLabel').innerText = 'Hide Scale';
         
     }
 
-    static hideScale(){
-        ScaleView.showScaleBoolean = false;
-        ScaleView.scaleTextView.innerText = '';
-        if(ScaleView.isKeyboardSvgShown){
-            ScaleView.resetKeyboardNotes();
+    hideScale(){
+        this.showScaleBoolean = false;
+        this.scaleTextView.innerText = '';
+        if(this.isKeyboardSvgShown){
+            this.resetKeyboardNotes();
         }
         else{
-            ScaleView.resetGuitarNotes();
+            this.resetGuitarNotes();
         }
         document.getElementById('showScaleLabel').innerText = 'Show Scale';
     }
     
-    static highlightKeyboardNotes(){
-        ScaleView.resetKeyboardNotes();
+    highlightKeyboardNotes(){
+        this.resetKeyboardNotes();
 
         //Marks notes in scale
-        let rootNoteKeyDouble = ScaleView.scaleClass.scaleNotes[0][0];
+        let rootNoteKeyDouble = this.scaleClass.scaleNotes[0][0];
         
-        ScaleView.scaleClass.scaleNotes.forEach(eachNote => {
+        this.scaleClass.scaleNotes.forEach(eachNote => {
             //c1 - h2 H=6.5 c1=1 d1<c1
            if(rootNoteKeyDouble <= eachNote[0]){
-            document.getElementById(eachNote[1].concat("1")).style.fill = '#21BF75';//ScaleView.noteColorMap.get(eachNote);
+            document.getElementById(eachNote[1].concat("1")).style.fill = '#21BF75';//this.noteColorMap.get(eachNote);
            }
            else{
-            document.getElementById(eachNote[1].concat("2")).style.fill = '#21BF75';//ScaleView.noteColorMap.get(eachNote);
+            document.getElementById(eachNote[1].concat("2")).style.fill = '#21BF75';//this.noteColorMap.get(eachNote);
            }
         });
     }
 
-    static resetKeyboardNotes(){
+    resetKeyboardNotes(){
         Scale.allNotesMap.forEach((eachNote, eachKey) => {
             let fill = '#ffffff';
             if([1.5, 2.5, 4, 5, 6].includes(eachKey)){
@@ -255,8 +279,8 @@ class ScaleView {
     }
 
 
-    static highlightGuitarNotes(){
-        ScaleView.resetGuitarNotes();
+    highlightGuitarNotes(){
+        this.resetGuitarNotes();
 
         //Every guitarString has it's own start value
         let guitarStringsMap = new Map();
@@ -268,7 +292,7 @@ class ScaleView {
         guitarStringsMap.set('e', 3);
 
         let scaleNotesDoubles = [];
-        ScaleView.scaleClass.scaleNotes.forEach(eachNote => {
+        this.scaleClass.scaleNotes.forEach(eachNote => {
             scaleNotesDoubles.push(eachNote[0]);
         });
         Array.from(guitarStringsMap.keys()).forEach(eachKey => {
@@ -282,57 +306,53 @@ class ScaleView {
                 }
             }
         })
-
     }
 
-    static resetGuitarNotes(){
+    resetGuitarNotes(){
         let guitarStrings = ['E', 'A','D','G','H','e']
 
         for(let i=0; i<guitarStrings.length; i++){
             for(let j=1; j<=5; j++){
                 document.getElementById(guitarStrings[i].concat(j.toString()).concat("Guitar")).style.visibility = "hidden";
             }
-        }
-        
+        } 
     }
 
-
-    initializeNoteColorMap(){
-        ScaleView.noteColorMap.set('C','#F26B8F');
-        ScaleView.noteColorMap.set('C#','#F1A7F2');
-        ScaleView.noteColorMap.set('D','#A2F2E4');
-        ScaleView.noteColorMap.set('D#','#F2B705');
-        ScaleView.noteColorMap.set('E','#F28705');
-        ScaleView.noteColorMap.set('F','#014040');
-        ScaleView.noteColorMap.set('F#','#1BA673');
-        ScaleView.noteColorMap.set('G','#21BF75');
-        ScaleView.noteColorMap.set('G#','#F2EFBB');
-        ScaleView.noteColorMap.set('A','#F29991');
-        ScaleView.noteColorMap.set('A#','#B57114');
-        ScaleView.noteColorMap.set('H','#962B09');
-      }
-
-    static showGuitarSVG(){
-       ScaleView.svgContainerDiv.removeChild(ScaleView.keyboardSVG);
-       ScaleView.svgContainerDiv.appendChild(ScaleView.guitarSVG);
+    showGuitarSVG(){
+       this.svgContainerDiv.removeChild(this.keyboardSVG);
+       this.svgContainerDiv.appendChild(this.guitarSVG);
     }
-    static showKeyboardSVG(){
-        ScaleView.svgContainerDiv.removeChild(ScaleView.guitarSVG);
-        ScaleView.svgContainerDiv.appendChild(ScaleView.keyboardSVG);
+    showKeyboardSVG(){
+        this.svgContainerDiv.removeChild(this.guitarSVG);
+        this.svgContainerDiv.appendChild(this.keyboardSVG);
     }
     
-    static swtichSVG(){
-        if(ScaleView.svgContainerDiv.firstElementChild == ScaleView.keyboardSVG){
-            ScaleView.showGuitarSVG();
-            ScaleView.isKeyboardSvgShown = false;
+    swtichSVG(){
+        if(this.svgContainerDiv.firstElementChild == this.keyboardSVG){
+            this.showGuitarSVG();
+            this.isKeyboardSvgShown = false;
         }
         else{
-            ScaleView.showKeyboardSVG();
-            ScaleView.isKeyboardSvgShown = true;
+            this.showKeyboardSVG();
+            this.isKeyboardSvgShown = true;
         }
     }
-}
 
+    initializeNoteColorMap(){
+        this.noteColorMap.set('C','#F26B8F');
+        this.noteColorMap.set('C#','#F1A7F2');
+        this.noteColorMap.set('D','#A2F2E4');
+        this.noteColorMap.set('D#','#F2B705');
+        this.noteColorMap.set('E','#F28705');
+        this.noteColorMap.set('F','#014040');
+        this.noteColorMap.set('F#','#1BA673');
+        this.noteColorMap.set('G','#21BF75');
+        this.noteColorMap.set('G#','#F2EFBB');
+        this.noteColorMap.set('A','#F29991');
+        this.noteColorMap.set('A#','#B57114');
+        this.noteColorMap.set('H','#962B09');
+      }
+}
 
 
 new ScaleView();
