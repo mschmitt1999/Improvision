db1bf9386d08540092eeaef600aa9df0b74e9eba
mschmitt1999 <majid.schmitt@gmail.com>
2024-04-15 11:08:49 +0200

    Too much changes

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 class Scale {
     static allNotesMap;
-    static scalesMap;
+    static allScaleMaps;
+    static modesMap;
+    scaleMap;
+    length;
+    halfSteps;
+    scaleNotesString;
     rootNoteKeyDouble;
     scaleNameString;
     scaleNotes = [];    
     
     constructor(scaleString, rootNoteKeyDouble){
-        
         Scale.initializeNotesMap();
-        Scale.initializeScalesMap();
-        this.setScaleNameString('Ionisch');
+        Scale.initializeAllScaleMaps();
+        Scale.initializeModesMap();
+        this.setScale('Dur');
         this.setRootNoteKeyDouble(1.0);
     }
 
@@ -33,55 +38,105 @@ class Scale {
     }
     
     
-    static initializeScalesMap(){
-        Scale.setScalesMap(new Map([
-            ['Ionisch', [7, 2, 6]],
-            ['Dorisch', [7, 1, 5]],
-            ['Phrygisch', [7, 0, 4]],
-            ['Lydisch', [7, 3, 7]],
-            ['Mixolydisch', [7, 2, 5]],
-            ['Äolisch', [7, 1, 4]],
-            ['Lokrisch', [7, 0, 3]],
-            ['DurPentatonik', [7, 2, 6]],
+    static initializeAllScaleMaps(){
+        Scale.setAllScaleMaps(new Map([
+            ['DurPentatonik', [7, 2, 6]], //False Length 7 should be 5 
             ['MolPentatonik', [7, 1, 4]],
+            ['Dur', [7, 2, 6]],
+            ['Mol', [7, 1, 4]]
         ]));
+    }
+
+    static initializeModesMap(){
+        Scale.setModesMap(new Map([ ['Ionisch', [7, 2, 6]],
+        ['Dorisch', [7, 1, 5]],
+        ['Phrygisch', [7, 0, 4]],
+        ['Lydisch', [7, 3, 7]],
+        ['Mixolydisch', [7, 2, 5]],
+        ['Äolisch', [7, 1, 4]],
+        ['Lokrisch', [7, 0, 3]]]));
     }
     
     calculateScale(){
         let noteKeyDouble = this.getRootNoteKeyDouble();
-        let lenghtOfScale = Scale.getScalesMap().get(this.getScaleNameString()).at(0);
-        let halfSteps = Scale.getScalesMap().get(this.getScaleNameString()).slice(1,lenghtOfScale);
         let scaleNotesString = '';
         this.setScaleNotes([]);
 
-        for (let i=0; i<lenghtOfScale; i++){
+        for (let i=0; i<this.getLength(); i++){
             if(!(this.getScaleNameString()=='DurPentatonik' && (i==3 || i==6)) && !(this.getScaleNameString()=='MolPentatonik' && (i==1 || i==5))){
                 scaleNotesString = scaleNotesString.concat(Scale.getAllNotesMap().get(noteKeyDouble),' | '); 
                 this.getScaleNotes().push([noteKeyDouble, Scale.getAllNotesMap().get(noteKeyDouble)]);
             }
-            if(halfSteps.includes(i)){
+            if(this.getHalfSteps().includes(i)){
                 noteKeyDouble += 0.5;
             }
             else{
                 noteKeyDouble +=1.0;
             }
-            if(noteKeyDouble >= lenghtOfScale){
-                noteKeyDouble -= lenghtOfScale-1;
+            if(noteKeyDouble >= this.getLength()){
+                noteKeyDouble -= this.getLength()-1;
             }
         }
-        return scaleNotesString;
+       
+        return this.calculateScaleString();
     }
 
     calculateScaleString(){
-        this.getScaleNotes.forEach(element => {
-            element[0]            
-        });
+        
+       let notesString = '';
+        let notesOrder = ['C','D','E','F','G','A','H'];
+        let difference;
+        let allNotesMapSwapped = new Map([ 
+            ['C', 1.0],
+            ['C#', 1.5],
+            ['D', 2.0],
+            ['D#', 2.5],
+            ['E', 3.0],
+            ['F', 3.5],
+            ['F#', 4.0],
+            ['G', 4.5],
+            ['G#', 5.0],
+            ['A', 5.5],
+            ['A#', 6.0],
+            ['H', 6.5]]);
+
+        let notes = this.getScaleNotes().slice();
+        let startingIndex = (this.getScaleNotes()[0][1].length == 1) ? notesOrder.indexOf(this.getScaleNotes()[0][1]) : notesOrder.indexOf(this.getScaleNotes()[0][1][0]);
+        if(this.getScaleNameString().includes('Pentatonik')){
+            notesOrder.splice((startingIndex + 6) % 7, 1);
+            notesOrder.splice((startingIndex + 3) % 7, 1);
+        }
+        for (let i = 0; i < notes.length; i++){
+                let note = notes[i][1];
+                let noteSuffix = '';
+                let index = (startingIndex + i) % notesOrder.length ;
+                if(note[0] != notesOrder[index]){
+                    if(note[0] == 'C' && notesOrder[index][0] == 'H'){
+                        notes[i][0] = 7;
+                    }
+                    difference = notes[i][0] - allNotesMapSwapped.get(notesOrder[index]);
+                    note = notesOrder[index];
+                    while(difference != 0){
+                        if(difference > 0){
+                            noteSuffix += '#';
+                            difference -= 0.5;
+                        }
+                        else{
+                            noteSuffix += 'b';
+                            difference += 0.5;
+                        }
+                    }
+                }
+                notesString += note +noteSuffix+ ' | ' 
+        }
+            
+        return notesString;
     }
 
     setAndCalculateRandomScale(){
-        let scaleKeyArray = Array.from(Scale.scalesMap.keys());
+        let scaleKeyArray = Array.from(Scale.allScaleMaps.keys());
         let noteKeyArray = Array.from(Scale.allNotesMap.keys());
-        this.setScaleNameString(scaleKeyArray.at(Math.floor(Math.random()*scaleKeyArray.length)));
+        this.setScale(scaleKeyArray.at(Math.floor(Math.random()*scaleKeyArray.length)));
         this.setRootNoteKeyDouble(noteKeyArray.at(Math.floor(Math.random() * noteKeyArray.length)));
         return this.calculateScale();  
     }
@@ -95,8 +150,11 @@ class Scale {
     getScaleNameString() {
         return this.scaleNameString;
     }
-    setScaleNameString(scaleNameString) {
+    setScale(scaleNameString) {
         this.scaleNameString = scaleNameString;
+        this.setScaleMap(Scale.getAllScaleMaps().get(this.getScaleNameString()));
+        this.setLength(this.getScaleMap().at(0));
+        this.setHalfSteps(this.getScaleMap().slice(1,this.getLength()));
     }
     getScaleNotes() {
         return this.scaleNotes;
@@ -104,17 +162,54 @@ class Scale {
     setScaleNotes(scaleNotes) {
         this.scaleNotes = scaleNotes;
     }
+
+    getScaleMap() {
+        return this.scaleMap;
+    }
+    setScaleMap(aMap) {
+        this.scaleMap = aMap;
+    }
+
+    getLength() {
+        return this.length;
+    }
+    setLength(anInteger) {
+        this.length = anInteger;
+    }
+
+    getHalfSteps() {
+        return this.halfSteps;
+    }
+    setHalfSteps(anArray) {
+        this.halfSteps = anArray;
+    }
+
+    getScaleNotesString() {
+        return this.scaleNotesString;
+    }
+    setScaleNotesString(aString) {
+        this.scaleNotesString = aString;
+    }
+
     static getAllNotesMap() {
         return Scale.allNotesMap;
     }
     static setAllNotesMap(notesMap) {
         Scale.allNotesMap = notesMap;
     }
-    static getScalesMap() {
-        return Scale.scalesMap;
+    static getAllScaleMaps() {
+        return Scale.allScaleMaps;
     }
-    static setScalesMap(scalesMap) {
-        Scale.scalesMap = scalesMap;
+    static setAllScaleMaps(allScaleMaps) {
+        Scale.allScaleMaps = allScaleMaps;
+    }
+
+    static getModesMap(){
+        return Scale.modesMap;
+    }
+
+    static setModesMap(modesMap){
+        Scale.modesMap = modesMap;
     }
 }
 
@@ -132,21 +227,22 @@ class ScaleView {
     showScaleBoolean;
     keyboardSVG;
     guitarSVG;
+    guitarFretBoardPosition;
     svgContainerDiv;
 
     constructor(){
         this.swtichSVGButton = document.getElementById('switchSVG');
-        this.isKeyboardSvgShown = true;
         this.showOrHideScaleButton = document.getElementById('showOrHideScaleButton');
         this.scalesSelectbox = document.getElementById('selectedScales');
         this.notesSelectbox = document.getElementById('selectedNotes');
         this.scaleTextView = document.getElementById('scaleTextView');
-        this.scaleClass = new Scale();
-        this.noteColorMap = new Map();
-        this.showScaleBoolean = false;
         this.keyboardSVG = document.getElementById('keyboardSVG');
         this.guitarSVG = document.getElementById('guitarSVG');
         this.svgContainerDiv = document.getElementById('svg-container');
+        this.isKeyboardSvgShown = true;
+        this.scaleClass = new Scale();
+        this.noteColorMap = new Map();
+        this.showScaleBoolean = false;
     
         this.showKeyboardSVG();
         this.initializeNoteColorMap();       
@@ -157,17 +253,19 @@ class ScaleView {
         this.showOrHideScaleButton.addEventListener('click', () => this.showOrHideScaleButtonClick());
         this.scalesSelectbox.onchange = () => this.scalesSelectboxOnchange();
         this.notesSelectbox.onchange = () => this.notesSelectboxOnchange();
+
+        this.guitarFretBoardPosition = 0;
     }
 
     scalesSelectboxOnchange(){
-        this.scaleClass.setScaleNameString(this.scalesSelectbox.value);
+        this.scaleClass.setScale(this.scalesSelectbox.value);
         if(this.showScaleBoolean){
             this.showScale();
         }   
     }
 
     notesSelectboxOnchange(){
-        this.scaleClass.rootNoteKeyDouble = parseFloat(this.notesSelectbox.value);
+        this.scaleClass.setRootNoteKeyDouble(parseFloat(this.notesSelectbox.value));
         if(this.showScaleBoolean){
             this.showScale();
         }
@@ -251,6 +349,7 @@ class ScaleView {
     }
     
     highlightKeyboardNotes(){
+        // Braucht eine andere Datenstruktur?
         this.resetKeyboardNotes();
 
         //Marks notes in scale
@@ -284,12 +383,12 @@ class ScaleView {
 
         //Every guitarString has it's own start value
         let guitarStringsMap = new Map();
-        guitarStringsMap.set('E', 3);
-        guitarStringsMap.set('A', 5.5);
-        guitarStringsMap.set('D', 2);
-        guitarStringsMap.set('G', 4.5);
-        guitarStringsMap.set('H', 6.5);
-        guitarStringsMap.set('e', 3);
+        guitarStringsMap.set('E', 3 + this.guitarFretBoardPosition);
+        guitarStringsMap.set('A', 5.5 + this.guitarFretBoardPosition);
+        guitarStringsMap.set('D', 2 + this.guitarFretBoardPosition) ;
+        guitarStringsMap.set('G', 4.5 + this.guitarFretBoardPosition);
+        guitarStringsMap.set('H', 6.5 + this.guitarFretBoardPosition);
+        guitarStringsMap.set('e', 3 + this.guitarFretBoardPosition);
 
         let scaleNotesDoubles = [];
         this.scaleClass.scaleNotes.forEach(eachNote => {
@@ -355,4 +454,4 @@ class ScaleView {
 }
 
 
-new ScaleView();
+let sv = new ScaleView();
